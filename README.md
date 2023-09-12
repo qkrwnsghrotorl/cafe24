d8239a4125cdefd36ab7e8fa01d4db08bfab2400
qkrwnsghrotorl <kyeongkeon0428@gmail.com>
2023-09-13 07:51:13 +0900

    git init

---
+++ README.md
@@ -0,0 +1,3 @@
+# sicfianlversion 3.0
+backend
+		
---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+// PORT
+const PORT = 3000;
+
+// module
+const express = require("express");
+const app = express();
+
+// 앱세팅
+app.set("views", "./views");
+app.set("view engine", "ejs"); 
+
+// 라우터 설정
+const home = require("./routes/home")
+
+// 미들웨어 method
+app.use("/", home);
+
+module.exports = app;
