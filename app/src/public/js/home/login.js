cccc99808055807ec176dc34ee6ccf38c779abcb
qkrwnsghrotorl <kyeongkeon0428@gmail.com>
2023-09-13 10:07:07 +0900

    DOM -> HTML Object

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hi");
-console.log("bye");
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginBtn = document.querySelector("button")
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+    console.log(req);
+}
