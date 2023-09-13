6428e936de44c5ee24df65e05b8108ec46d8da88
qkrwnsghrotorl <kyeongkeon0428@gmail.com>
2023-09-13 10:18:07 +0900

    bodyparser

--- app/app.js
+++ app/app.js
@@ -2,14 +2,20 @@
 // PORT
 const PORT = 3000;
 
-// module
+// module - express
 const express = require("express");
 const app = express();
+
+// module - bodyparser
+const bodyParser = require("body-parser");
 
 // 앱세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs"); 
 app.use(express.static(`${__dirname}/src/public`));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // 라우터 설정
 const home = require("./src/routes/home")
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,20 @@
 "use strict";
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
+}
 
-const home = (req, res) => {
-    res.render("home/index");
-};
-
-const login = (req, res) => {
-    res.render("home/login");
+const process = {
+    login: (req, res) => {
+        console.log(req.body);
+    },
 };
 
 module.exports = {
-    home,
-    login,
+    output,
+    process
 };
