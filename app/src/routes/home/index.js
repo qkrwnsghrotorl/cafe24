15adc4c3a13425b14b1844078ae69c1cad504c5c
qkrwnsghrotorl <kyeongkeon0428@gmail.com>
2023-09-14 17:45:35 +0900

    회원가입 화면구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,5 +8,6 @@ const ctrl = require("./home.ctrl")
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.post("/login", ctrl.process.login);
+router.get("/register", ctrl.output.register);
 
 module.exports = router;
